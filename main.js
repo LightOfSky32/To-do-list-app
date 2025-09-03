b812dca1205e9b9ac2dd7464eb99fddcd5459419
Sky's light <preciousarts2478@gmail.com>
2025-09-03 15:26:12 -0700

    worked more on the javascript parts

--- main.js
+++ main.js
@@ -4,10 +4,31 @@ const list = document.getElementById("taskList");
 const tasks = [];
 
 button.addEventListener("click", () => {
+    // learn how to check for empty strings first 
+
+    const message = document.getElementById("message");
+    if (textInput.value.trim() === "") {
+        message.textContent = "please input text";
+        setTimeout(() =>{
+            message.textContent ="";
+        }, 2000)
+        return;
+    } 
+
+    //done. rest of the code
     const userInput = textInput.value;
     const newTask = document.createElement("li");
     newTask.textContent = userInput;
-    
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "delete task";
+    removeButton.addEventListener("click", () => {
+        removeButton.parentElement.remove();
+    })
+
+
+    newTask.appendChild(removeButton);
     list.appendChild(newTask);
     textInput.value = "";
+
 })
